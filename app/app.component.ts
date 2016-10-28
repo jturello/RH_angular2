c2c6b08d780a2833efc5a6d2936871686a581c66
James Turello <jturello@yahoo.com>
2016-10-28 05:58:01 -0700

    refactor date and events objects and display

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,66 +1,45 @@
 import { Component } from '@angular/core';
+import { Event } from './event.model';
+import { Patient } from './patient.model';
 
-// export class Patient {
-//   id: number;
-//   name: string;
-// }
+var patient01 = new Patient(1, 'Joe Schmoe', 'AML with myelodysplasia');
 
-
-
-export class Event {
-  name: string;
-  patient: string;
-  problem: string;
-  date: Date;
-  // start_data: Date;
-  // end_date: Date;
-}
-
-
-var jan0116 = new Date(2016,1,1);
-var jan0516 = new Date(2016,1,5);
-var jan0616 = new Date(2016,1,6);
-var jan1016 = new Date(2016,1,10);
-var jan1216 = new Date(2016,1,12);
-var jan1516 = new Date(2016,1,15);
-var jan3116 = new Date(2016,1,31)
+var jan0116 = new Date(2016, 0, 1);
+var jan0516 = new Date(2016, 0, 5);
+var jan0616 = new Date(2016, 0, 6);
+var jan1016 = new Date(2016, 0, 10);
+var jan1216 = new Date(2016, 0, 12);
+var jan1516 = new Date(2016, 0, 15);
+var jan3116 = new Date(2016, 0, 31);
 
 const EVENTS: Event[] = [
-    {name: 'event01', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan0116}
-    , {name: 'event02', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan0516}
-    , {name: 'event03', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan0516}
-    , {name: 'event04', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan0616}
-    , {name: 'event05', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan1016}
-    , {name: 'event06', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan1216}
-    , {name: 'event07', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan1216}
-    , {name: 'event08', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan1516}
-    , {name: 'event09', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan1516}
-    , {name: 'event10', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan1516}
-    , {name: 'event11', patient: 'joe schmoe', problem: 'AML with myelodysplasia', date: jan3116}
-    //, {name: 'event01b', patient: 'jane doe', problem: 'diatetes myallitus'}
-    // , {name: 'event02b', patient: 'jane doe', problem: 'diatetes myallitus'}
-    // , {name: 'event03b', patient: 'jane doe', problem: 'diatetes myallitus'}
-    // , {name: 'event04b', patient: 'jane doe', problem: 'diatetes myallitus'}
-    // , {name: 'event05b', patient: 'jane doe', problem: 'diatetes myallitus'}
+    {name: 'event01', patient: patient01, date: jan0116}
+    , {name: 'event02', patient: patient01, date: jan0516}
+    , {name: 'event03', patient: patient01, date: jan0516}
+    , {name: 'event04', patient: patient01, date: jan0616}
+    , {name: 'event05', patient: patient01, date: jan1016}
+    , {name: 'event06', patient: patient01, date: jan1216}
+    , {name: 'event07', patient: patient01, date: jan1216}
+    , {name: 'event08', patient: patient01, date: jan1516}
+    , {name: 'event09', patient: patient01, date: jan1516}
+    , {name: 'event10', patient: patient01, date: jan1516}
+    , {name: 'event11', patient: patient01, date: jan3116}
   ]
-    // constructor(public patient_id: number, public patient_name: string, public problem: string, public event: string, public period_start?: Date, public period_end?: Date ) {
-  //   this.patient_id = patient_id;
-  //   this.patient_name = patient_name;
-  //   this.problem = problem;
-  //   this.event = event;
-  //   if (period_start && period_end) {
-  //     this.period_start = period_start;
-  //     this.period_end = period_end;
-  //   } else {
-  //     period_start = period_end = null;
-  //   }
-  // }
-// }
+
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>Grand Rounds Event Timeline</h1>
+
+    <h2>Events</h2>
+
+    <ul class="events">
+      <li *ngFor="let event of events">
+        {{event.patient.name}}, {{event.patient.problem}}, {{event.name}}, {{event.date.getMonth() + 1}}/{{event.date.getDate()}}/{{event.date.getFullYear()}}
+      </li>
+    </ul>
+
     <!--
         <form>
         <div class="row">
@@ -83,12 +62,7 @@ const EVENTS: Event[] = [
           </div>
       </form>
       -->
-      <h2>Events</h2>
-      <ul class="events">
-        <li *ngFor="let event of events" style="list-style-type: none;">
-          {{event.patient}}, {{event.problem}}, {{event.name}}, {{event.date}}
-        </li>
-      </ul>
+
   `
 })
 
