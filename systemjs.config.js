073f5270923b94ac237667877bc487350524f0c8
James Turello <jturello@yahoo.com>
2016-10-31 13:56:35 -0700

    fix incorrect setup from online tutorial

--- systemjs.config.js
+++ systemjs.config.js
@@ -23,7 +23,9 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      // 'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -35,7 +37,7 @@
         defaultExtension: 'js'
       },
       'angular-in-memory-web-api': {
-        main: './index.js',
+        // main: './index.js',
         defaultExtension: 'js'
       }
     }
